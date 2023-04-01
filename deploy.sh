d92175b8869d47df26ddb2bcdab243f87ee7bda9
wchr-aun <wchr.aun@gmail.com>
2023-04-02 00:28:54 +0100

    ci(workflows): update workflow

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kill $(ps aux | grep -i '[p]ython3 main.py' | awk '{print $2}')
-cd discord-bot-exchange-rate
-rm nohup.out
-git pull
-pip install -r requirements.txt
-nohup python3 main.py &
